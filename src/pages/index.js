e8ce7793c58923bb9202e7869df9a88eaea463b7
huongnguyenduc <duchuong007@gmail.com>
2022-12-23 16:15:25 +0700

    feat: add dotenv

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,25 @@
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import {Octokit} from '@octokit/core';
 import React from 'react';
+import siteConfig from '@generated/docusaurus.config';
 
 const Index = () => {
-    const {siteConfig} = useDocusaurusContext();
-const [loading, setLoading] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
     const handleInvokeBuild = async () => {
-    setLoading(true);
-    try {
-        const octokit = new Octokit({
-            auth: siteConfig.REACT_APP_PAS
-        });
+        setLoading(true);
+        try {
+            const octokit = new Octokit({
+                auth: `${siteConfig.customFields.REACT_APP_PAS}`
+            });
 
-        const result = await octokit.request('POST /repos/huongnguyenduc/docu-notion/actions/workflows/43724788/dispatches', {
-            owner: 'huongnguyenduc',
-            repo: 'docu-notion',
-            workflow_id: '43724788',
-            ref: 'main',
-            // inputs: {
-            //     name: 'Mona the Octocat',
-            //     home: 'San Francisco, CA'
-            // }
-        });
-        setLoading(false);
-        console.log("result", result);
-    } catch (error) {
-        console.log(error);
-        setLoading(false);
-    }
+            const result = await octokit.request('POST /repos/huongnguyenduc/docu-notion/actions/workflows/release.yml/dispatches', {
+                ref: 'main',
+            });
+            setLoading(false);
+            console.log("result", result);
+        } catch (error) {
+            console.log(error);
+            setLoading(false);
+        }
     }
     return (
         <div>
