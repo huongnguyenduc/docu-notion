255dd0be33379b561f3e9b48b858291425efde8b
huongnguyenduc <duchuong007@gmail.com>
2022-12-27 11:55:18 +0700

    chore: update base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   title: "Docusaurus x Notion",
   tagline: "",
   url: "https://huongnguyenduc.github.io",
-  baseUrl: "/",
+  baseUrl: "/docu-notion/",
   onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
